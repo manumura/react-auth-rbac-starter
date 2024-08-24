56acc5e5174623270ba66090d742201616e2c025
Emmanuel Mura <emmanuel.mura@gmail.com>
2024-08-24 14:26:32 +0700

    register

--- src/routes/register.tsx
+++ src/routes/register.tsx
@@ -1,5 +1,5 @@
 import { AxiosError } from 'axios';
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useGoogleReCaptcha } from 'react-google-recaptcha-v3';
 import { FormProvider, useForm } from 'react-hook-form';
 import {
@@ -13,14 +13,12 @@ import {
 } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import FormInput from '../components/FormInput';
-import { login, validateRecaptcha } from '../lib/api';
-import { getUserFromIdToken } from '../lib/jwt.utils';
-import { saveAuthentication } from '../lib/storage';
-import useUserStore from '../lib/user-store';
-import { IUser, LoginResponse } from '../types/custom-types';
+import { register, validateRecaptcha } from '../lib/api';
+import { IUser } from '../types/custom-types';
 
 export const action = async ({ request }: { request: Request }) => {
   const formData = await request.formData();
+  const name = formData.get('name') as string;
   const email = formData.get('email') as string;
   const password = formData.get('password') as string;
   const token = formData.get('token') as string;
@@ -35,37 +33,27 @@ export const action = async ({ request }: { request: Request }) => {
       throw new Error('Captcha validation failed');
     }
   } catch (error) {
+    console.error('Captcha validation failed', error);
     if (error instanceof AxiosError && error.response?.data.message) {
       throw new Error(error.response.data.message);
     }
     throw error;
   }
 
-  let response: LoginResponse;
+  let user: IUser;
   try {
-    response = await login(email, password);
+    user = await register(email, password, name);
   } catch (error) {
+    console.error('Captcha validation failed2', error);
     if (error instanceof AxiosError && error.response?.data.message) {
       throw new Error(error.response.data.message);
     }
     throw error;
   }
 
-  if (!response) {
-    throw new Error('Invalid response');
-  }
-
-  const { accessToken, refreshToken, idToken } = response;
-  if (!idToken || !accessToken || !refreshToken) {
-    throw new Error('Invalid response');
-  }
-
-  saveAuthentication(accessToken, refreshToken, idToken);
-  const user = await getUserFromIdToken(idToken);
   if (!user) {
-    throw new Error('Invalid user');
+    throw new Error('Invalid response');
   }
-
   return user;
 };
 
@@ -83,24 +71,23 @@ function SubmitButton({
       className='btn btn-primary w-full'
       onClick={(event) => onSubmit(event)}
     >
-      Login
+      Register
     </button>
   );
   const btnDisabled = (
-    <button className='btn btn-disabled btn-primary w-full'>Login</button>
+    <button className='btn btn-disabled btn-primary w-full'>Register</button>
   );
   const btnLoading = (
     <button className='btn btn-disabled btn-primary w-full'>
       <span className='loading loading-spinner'></span>
-      Login
+      Register
     </button>
   );
 
   return !isValid ? btnDisabled : isLoading ? btnLoading : btn;
 }
 
-export default function Login(): React.ReactElement {
-  const userStore = useUserStore();
+export default function Register(): React.ReactElement {
   const navigation = useNavigation();
   const navigate = useNavigate();
   const user = useActionData() as IUser;
@@ -118,12 +105,12 @@ export default function Login(): React.ReactElement {
       return;
     }
     const token = await executeRecaptcha('onSubmit');
-    const { name, email, password } = getValues();
+    const { email, password, name } = getValues();
 
     const formData = new FormData();
-    formData.append('name', name);
     formData.append('email', email);
     formData.append('password', password);
+    formData.append('name', name);
     formData.append('token', token);
     submit(formData, { method: 'post' });
   };
@@ -139,41 +126,125 @@ export default function Login(): React.ReactElement {
 
   useEffect(() => {
     if (user) {
-      userStore.setUser(user);
-
-      toast(`Welcome ${user?.name}!`, {
+      toast(`Registration successful ${user?.name}!`, {
         type: 'success',
         position: 'bottom-right',
       });
-      navigate('/');
+      navigate('/login');
     }
   }, [user]);
 
   const methods = useForm({
     mode: 'all',
   });
+
   const {
     getValues,
     formState: { isValid },
+    watch,
   } = methods;
 
+  //   const mutation = useMutation({
+  //     mutationFn: ({
+  //       email,
+  //       password,
+  //       name,
+  //     }: {
+  //       email: string;
+  //       password: string;
+  //       name: string;
+  //     }) => onMutate(email, password, name),
+  //     async onSuccess(user, variables, context) {
+  //       toast(`Registration successful ${user?.name}!`, {
+  //         type: 'success',
+  //         position: 'bottom-right',
+  //       });
+
+  //       router.push('/login');
+  //     },
+  //     onError(error, variables, context) {
+  //       toast(error?.message, {
+  //         type: 'error',
+  //         position: 'bottom-right',
+  //       });
+  //     },
+  //   });
+
+  //   const onMutate = async (email, password, name): Promise<IUser> => {
+  // if (!executeRecaptcha) {
+  //   throw new Error('Recaptcha not loaded');
+  // }
+  // const token = await executeRecaptcha('onSubmit');
+  // const isCaptchaValid = await validateRecaptcha(token);
+  // if (!isCaptchaValid) {
+  //   throw new Error('Captcha validation failed');
+  // }
+
+  // let response: AxiosResponse<IUser>;
+  // try {
+  //   response = await register(email, password, name);
+  // } catch (error) {
+  //   if (error?.response) {
+  //     throw new Error(error.response.data.message);
+  //   }
+  //   throw new Error(error.message);
+  // }
+  // return response.data;
+  //   };
+
+  //   const onSubmit = async (formData): Promise<void> => {
+  //     if (!formData) {
+  //       return;
+  //     }
+
+  //     mutation.mutate({
+  //       email: formData.email,
+  //       password: formData.password,
+  //       name: formData.name,
+  //     });
+  //   };
+
+  const nameConstraints = {
+    required: { value: true, message: 'Full Name is required' },
+    minLength: {
+      value: 5,
+      message: 'Full Name is min 5 characters',
+    },
+  };
   const emailConstraints = {
     required: { value: true, message: 'Email is required' },
   };
   const passwordConstraints = {
     required: { value: true, message: 'Password is required' },
+    minLength: {
+      value: 8,
+      message: 'Password is min 8 characters',
+    },
+  };
+  const passwordConfirmConstraints = {
+    required: { value: true, message: 'Confirm Password is required' },
+    validate: (value: string): string | undefined => {
+      if (watch('password') !== value) {
+        return 'Passwords do no match';
+      }
+    },
   };
 
   return (
-    <section className='h-section py-20 w-full bg-slate-200'>
+    <section className='h-section w-full py-20 bg-slate-200'>
       <FormProvider {...methods}>
         <Form
-          id='login-form'
-          className='mx-auto w-full max-w-md space-y-5 p-8 overflow-hidden rounded-2xl bg-slate-50 shadow-lg'
+          id='register-form'
+          className='mx-auto w-full max-w-md space-y-5 overflow-hidden rounded-2xl bg-slate-50 p-8 shadow-lg'
         >
           <h1 className='mb-4 text-center text-4xl font-[600]'>
-            Login to MyApp
+            Register to MyApp!
           </h1>
+          <FormInput
+            label='Full Name'
+            name='name'
+            constraints={nameConstraints}
+          />
           <FormInput
             label='Email'
             name='email'
@@ -186,24 +257,23 @@ export default function Login(): React.ReactElement {
             type='password'
             constraints={passwordConstraints}
           />
-
-          <div className='text-right'>
-            <Link to='/forgot-password' className='text-secondary'>
-              Forgot Password?
+          <FormInput
+            label='Confirm Password'
+            name='passwordConfirm'
+            type='password'
+            constraints={passwordConfirmConstraints}
+          />
+          <span className='block'>
+            Already have an account?{' '}
+            <Link to='/login' className='text-secondary'>
+              Login Here
             </Link>
-          </div>
-
+          </span>
           <SubmitButton
             isValid={isValid}
             isLoading={isLoading}
             onSubmit={onSubmit}
           />
-          <span className='block'>
-            Need an account?{' '}
-            <Link to='/register' className='text-secondary'>
-              Sign Up Here
-            </Link>
-          </span>
         </Form>
       </FormProvider>
     </section>
