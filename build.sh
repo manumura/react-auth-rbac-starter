41c7eab311aa48c11ef96790de9f8a3fae3d7b4c
Emmanuel Mura <emmanuel.mura@gmail.com>
2024-10-22 19:45:17 +0700

    build script

--- build.sh
+++ build.sh
@@ -1,26 +1,19 @@
 #!/bin/sh
 echo "Environment to build: $1"
 
-if [ "$1" = "dev" ]; then
-    echo "Building for development"
-    cp .env.dev .env.production
-
-    docker build -t manumura/nginx-react-auth-rbac-starter:dev .
-
-    docker push manumura/nginx-react-auth-rbac-starter:dev
-
-elif [ "$1" = "prod" ]; then
-    echo "Building for production"
-    cp .env.prod .env.production
-
-    docker build -t manumura/nginx-react-auth-rbac-starter:prod .
-
-    docker push manumura/nginx-react-auth-rbac-starter:prod
-
-else
+if [ "$1" != "dev" ] && [ "$1" != "prod" ]; then
     echo "Invalid environment: should pass dev or prod as argument"
     exit 1
 fi
 
-# docker run -d --rm -p 80:80 --name nginx-react-auth-rbac-starter manumura/nginx-react-auth-rbac-starter:dev
+echo "Building for $1"
+
+cp .env.$1 .env.production
+
+docker build -t manumura/nginx-react-auth-rbac-starter:$1 .
+
 rm .env.production
+
+docker push manumura/nginx-react-auth-rbac-starter:$1
+
+# docker run -d --rm -p 80:80 --name nginx-react-auth-rbac-starter manumura/nginx-react-auth-rbac-starter:$1
