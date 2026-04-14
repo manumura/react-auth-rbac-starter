53123e11b729e14aae0dcf998c4403e26b0dcfeb
manumura <emmanuel.mura@gmail.com>
2026-04-14 09:30:22 +0700

    refactor logout + navbar

--- src/error-page-with-header.tsx
+++ src/error-page-with-header.tsx
@@ -1,26 +1,26 @@
-import { useRouteError, useRouteLoaderData } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import { IAuthenticatedUser } from './types/custom-types';
+import { useRouteError } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 export default function ErrorPageWithHeader() {
-  const currentUser = useRouteLoaderData('root') as IAuthenticatedUser | null;
   const error: any = useRouteError();
   console.error(error);
 
   return (
     <>
-    <Navbar user={currentUser} />
-    <section id='error-page' className='h-section bg-slate-200 pt-20'>
-      <div className='mx-auto flex h-[20rem] max-w-4xl flex-col items-center justify-center rounded-md bg-slate-50'>
-        <div>
-          <h1 className='text-2xl font-semibold'>Oops!</h1>
-          <p className='text-xl font-semibold'>Sorry, an unexpected error has occurred.</p>
-          <p className='text-xl font-semibold'>
-            <i>{error.statusText || error.message}</i>
-          </p>
+      <Navbar />
+      <section id="error-page" className="h-section bg-slate-200 pt-20">
+        <div className="mx-auto flex h-[20rem] max-w-4xl flex-col items-center justify-center rounded-md bg-slate-50">
+          <div>
+            <h1 className="text-2xl font-semibold">Oops!</h1>
+            <p className="text-xl font-semibold">
+              Sorry, an unexpected error has occurred.
+            </p>
+            <p className="text-xl font-semibold">
+              <i>{error.statusText || error.message}</i>
+            </p>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
     </>
   );
 }
